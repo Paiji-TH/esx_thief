416eb5e069560d0ef94a99deb9f6274ff240eaf4
rodrii-rms <35659055+rodrii-rms@users.noreply.github.com>
2018-11-22 08:17:46 -0300

    Add files via upload

---
+++ locales/es.lua
@@ -0,0 +1,22 @@
+Locales['es'] = {
+    ['no_hands_up']         = 'EL sujeto no tiene las manos arriba!',
+    ['cash']                = 'Dinero',
+    ['black_money']         = 'Dinero Sucio',
+    ['inventory']           = 'Inventario',
+    ['target_inventory']    = 'Inventario del Sujeto',
+    ['steal']               = 'Robar',
+    ['return']              = 'Volver',
+    ['action_choice']       = 'Eleguir accion',
+    ['amount']              = 'Cantidad',
+    ['too_far']             = 'Estás demasiado lejos para hacer eso!',
+    ['you_far_away']        = 'Te has alejado demasiado!',
+    ['many_people_nearby']  = 'Mucha gente cerca para hacer esto!', 
+    ['no_players_nearby']   = 'No hay sujeto cerca',
+    ['ex_inv_lim_source']   = 'No tienes suficiente espacio en tu inventario',
+    ['ex_inv_lim_target']   = 'El sujeto no tiene suficiente espacio',
+    ['you_stole']           = 'Robaste',
+    ['from_your_target']    = 'de',
+    ['someone_stole']       = 'Alguien te robo',
+    ['invalid_quantity']    = 'Cantidad Invalida',
+}
+    
